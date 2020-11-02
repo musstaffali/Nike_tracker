a97b615c68fc72060257543e29d914a3d106cdc6
Mustafa Mustafa <mamustafa@gtcc.edu>
2020-11-02 12:30:57 -0500

    final update

--- routes/nike-routes.js
+++ routes/nike-routes.js
@@ -1,33 +1,23 @@
 const memory = require("../models");
 
-
 module.exports = function (doit) {
 
     doit.get("/api/workouts", (req, res) => {
-
         memory.Workout.find({}).then(memoryWorkout => {
-
             memoryWorkout.forEach(workout => {
                 var total = 0;
                 workout.exercises.forEach(e => {
                     total += e.duration;
                 });
                 workout.totalDuration = total;
-
             });
-
             res.json(memoryWorkout);
         }).catch(err => {
             res.json(err);
         });
     });
 
-
-
-
-    // add exercise using id
     doit.put("/api/workouts/:id", (req, res) => {
-
         memory.Workout.updateOne(
             { _id: req.params.id },
             {
@@ -39,13 +29,10 @@ module.exports = function (doit) {
             }).catch(err => {
                 res.json(err);
             });
-
     });
 
     //create workout
     doit.post("/api/workouts", ({ body }, res) => {
-
-
         memory.Workout.create(body).then((memoryWorkout => {
             res.json(memoryWorkout);
         })).catch(err => {
@@ -53,19 +40,13 @@ module.exports = function (doit) {
         });
     });
 
-
     doit.get("/api/workouts/range", (req, res) => {
-
         memory.Workout.find({}).then(memoryWorkout => {
             console.log("ALL WORKOUTS");
             console.log(memoryWorkout);
-
             res.json(memoryWorkout);
         }).catch(err => {
             res.json(err);
         });
-
     });
-
-
 }
