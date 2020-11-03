2479e548744600c4b30927945a4d3e824593c750
Mustafa Mustafa <mamustafa@gtcc.edu>
2020-11-03 09:25:58 -0500

    server.js

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
-
 require("./routes/nike-routes.js")(app);
 
 
